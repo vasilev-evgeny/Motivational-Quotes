6763b24bc07f8ae07ad77fd220b3a095f8aaa599
vasiliev-evgeny <vasiliev.creativnoemilo99@gmail.com>
2025-07-28 17:15:51 +0300

    Initial Commit

---
+++ Motivational_Quotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Motivational_Quotes
+//
+//  Created by Евгений Васильев on 28.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
